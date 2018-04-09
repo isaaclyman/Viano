de4d744395a2cba0060b13c1e6ca8b6ebe4c871a
Isaac Lyman <isaaclyman@gmail.com>
2018-04-08 19:37:48 -0600

    Make La Cucaracha more readable

--- README.md
+++ README.md
@@ -28,18 +28,19 @@ The first line of "La Cucaracha" in Viano looks like this:
 <Song title="La cucaracha" :tempo="200" :time-signature="[4, 4]">
   <Part :measure="0">
     <Sequence>
-      <Note name="c4" value="1/8" :repeat="3">La cuca</Note>
-      <Note name="f4" value="3/8">ra</Note>
-      <Note name="a4" value="1/4">cha</Note>
-      <Note name="c4" value="1/8" :repeat="3">La cuca</Note>
-      <Note name="f4" value="3/8">ra</Note>
-      <Note name="a4" value="1/4">cha</Note>
-      <Rest value="3/8"></Rest>
-      <Note name="f4" value="1/4">ya</Note>
-      <Note name="f4" value="1/8">no</Note>
-      <Note name="e4" value="1/8" :repeat="2">puede</Note>
-      <Note name="d4" value="1/8" :repeat="2">cami</Note>
-      <Note name="c4" value="3/8">nar</Note>
+      <Note name="c4" value="1/8" :repeat="3">          La cuca
+      </Note><Note name="f4" value="3/8">               ra
+      </Note><Note name="a4" value="1/4">               cha
+      </Note><Note name="c4" value="1/8" :repeat="3">   La cuca
+      </Note><Note name="f4" value="3/8">               ra
+      </Note><Note name="a4" value="1/4">               cha
+      </Note><Rest value="3/8"></Rest>
+      <Note name="f4" value="1/4">                      ya
+      </Note><Note name="f4" value="1/8">               no
+      </Note><Note name="e4" value="1/8" :repeat="2">   puede
+      </Note><Note name="d4" value="1/8" :repeat="2">   cami-
+      </Note><Note name="c4" value="3/8">               nar
+      </Note>
     </Sequence>
   </Part>
 </Song>
--- src/examples/LaCucaracha.vue
+++ src/examples/LaCucaracha.vue
@@ -2,18 +2,19 @@
   <Song title="La cucaracha" :tempo="200" :time-signature="[4, 4]">
     <Part :measure="0">
       <Sequence>
-        <Note name="c4" value="1/8" :repeat="3">La cuca</Note>
-        <Note name="f4" value="3/8">ra</Note>
-        <Note name="a4" value="1/4">cha</Note>
-        <Note name="c4" value="1/8" :repeat="3">La cuca</Note>
-        <Note name="f4" value="3/8">ra</Note>
-        <Note name="a4" value="1/4">cha</Note>
-        <Rest value="3/8"></Rest>
-        <Note name="f4" value="1/4">ya</Note>
-        <Note name="f4" value="1/8">no</Note>
-        <Note name="e4" value="1/8" :repeat="2">puede</Note>
-        <Note name="d4" value="1/8" :repeat="2">cami</Note>
-        <Note name="c4" value="3/8">nar</Note>
+        <Note name="c4" value="1/8" :repeat="3">          La cuca
+        </Note><Note name="f4" value="3/8">               ra
+        </Note><Note name="a4" value="1/4">               cha
+        </Note><Note name="c4" value="1/8" :repeat="3">   La cuca
+        </Note><Note name="f4" value="3/8">               ra
+        </Note><Note name="a4" value="1/4">               cha
+        </Note><Rest value="3/8"></Rest>
+        <Note name="f4" value="1/4">                      ya
+        </Note><Note name="f4" value="1/8">               no
+        </Note><Note name="e4" value="1/8" :repeat="2">   puede
+        </Note><Note name="d4" value="1/8" :repeat="2">   cami-
+        </Note><Note name="c4" value="3/8">               nar
+        </Note>
       </Sequence>
     </Part>
   </Song>
